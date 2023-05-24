ab98b7d4e40c7166b6bb6f491e5c61e84193a773
GaneshP0296 <ganeshp457@gmail.com>
2023-05-24 15:18:28 +0530

    Day 3- Line comparison

---
+++ LineComp.java
@@ -0,0 +1,34 @@
+import java.lang.Integer;
+
+public class LineComp {
+    Integer disCast = null;
+    Integer disCast1 = null;
+    double distance(int x1, int y1, int x2, int y2)
+    {
+        // Calculating distance
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+    public void compare() {
+        int comp = disCast.compareTo(disCast1);
+        if(comp > 0) {
+            System.out.println("line:1 is greater than line:2");
+        } else if(comp< 0) {
+            System.out.println("line:1 is less than line:2");
+        } else {
+            System.out.println("line:1 is equal to line:2");
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison program");
+        LineComp linecomp = new LineComp();
+        int dis =(int) linecomp.distance(5,4,8,6);
+        linecomp.disCast = Integer.valueOf(dis);
+        System.out.println("distance ::"+dis);
+        int dis1= (int) linecomp.distance(3,6,8,7);
+        linecomp.disCast1 = Integer.valueOf(dis1);
+        System.out.println("distance ::"+dis1);
+        //System.out.println(Objects.equals(dis, dis1))
+        linecomp.compare();
+
+    }
+}
